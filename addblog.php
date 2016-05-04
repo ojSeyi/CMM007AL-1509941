fd4bf056e4fc8c294b9c9c9884bc6e65919a3945
ojSeyi <1509941@rgu.ac.uk>
2016-05-04 12:28:16 +0100

    2nd commit

--- addblog.php
+++ addblog.php
@@ -7,7 +7,7 @@ if(empty($_POST['title']) || empty($_POST['summary']) || empty($_POST['category'
     $category = $_POST['category'];
     $summary = $_POST['summary'];
     $author = $_POST['author'];
-    $bloginput = "INSERT INTO bugs(entryTitle, entrySummary, category, submitter) VALUES ('$title', '$summary', '$category', '$author')";
+    $bloginput = "INSERT INTO blogView(entryTitle, entrySummary, category, submitter) VALUES ('$title', '$summary', '$category', '$author')";
     $result = mysqli_query($db, $bloginput);
     header("Location: blog.php");
 }
