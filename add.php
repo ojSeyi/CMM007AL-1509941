92707e5ff9d0b3426b79c342e0ab97dde34b15df
ojSeyi <1509941@rgu.ac.uk>
2016-05-04 11:38:41 +0100

    2nd commit

--- add.php
+++ add.php
@@ -1,7 +1,57 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1509941
- * Date: 04/05/2016
- * Time: 11:32
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="Assets/style.css">
+    <link rel="stylesheet" href="Assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu|Montserrat|Passion+One|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+        initial-scale=1,
+        minimum-scale=1,
+        maximum-scale=1"/>
+</head>
+<body>
+<header>
+    <div id="title">
+        <h1>myBlog</h1>
+        <h2>because the internet needs to know what i think</h2>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="blog.html">All Blog Items</a></li>
+            <li><a href="blog.html">Work Items</a></li>
+            <li><a href="blog.html">University Items</a></li>
+            <li><a href="blog.html">Family Items</a></li>
+            <li><a href="add.html">Insert a Blog Item</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <div  id="form">
+        <form>
+            <label>Enter Title:</label>
+            <input type="text" name="title" required><br><br>
+            <label>Enter Summary:</label>
+            <textarea name="summary" rows="4" cols="30" placeholder="Enter text here..." required></textarea><br><br>
+            <label>Enter Category:</label>
+            <select name="category" size="1" required>
+                <option>Work</option>
+                <option>University</option>
+                <option>Family</option>
+            </select><br><br>
+            <label>Submitted By:</label>
+            <input type="text" name="author" required><br><br>
+            <input type="submit" value="submit">
+        </form>
+    </div>
+</main>
+
+<footer>
+    <p> Designed by Oluwaseyi Nojimu-Yusuf, 2016</p>
+</footer>
+
+</body>
+</html>
--- blog.php
+++ blog.php
@@ -1,7 +1,48 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1509941
- * Date: 04/05/2016
- * Time: 11:32
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="Assets/style.css">
+    <link rel="stylesheet" href="Assets/unsemantic-grid-responsive-tablet.css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu|Montserrat|Passion+One|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+    <meta name="viewpoint"
+          content="width=device-width,
+        initial-scale=1,
+        minimum-scale=1,
+        maximum-scale=1"/>
+</head>
+<body>
+<header>
+    <div id="title">
+        <h1>myBlog</h1>
+        <h2>because the internet needs to know what i think</h2>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="blog.html">All Blog Items</a></li>
+            <li><a href="blog.html">Work Items</a></li>
+            <li><a href="blog.html">University Items</a></li>
+            <li><a href="blog.html">Family Items</a></li>
+            <li><a href="add.html">Insert a Blog Item</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <section>
+        <div id="blogitem">
+            <h3>Bugs by Michael Bolton</h3>
+            <h4>Work </h4>
+            <p>Bug-fright is the collective name for a group of software bugs, that affect versions 2.2 and newer of the Android operating system, allowing an attacker to perform arbitrary operations on the victim device through remote code execution and privilege escalation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae rutrum ligula, quis blandit eros. Curabitur et pulvinar felis. Duis molestie sit amet eros sit amet viverra. Aenean sed dolor eleifend, dictum ex ac, suscipit libero. Nunc sit amet lectus non elit dictum dictum.</p>
+        </div>
+    </section>
+
+</main>
+
+<footer>
+    <p> Designed by Oluwaseyi Nojimu-Yusuf, 2016</p>
+</footer>
+
+</body>
+</html>
